82a51f16ab8929fecaf8ba99c029add5540a8680
Phillip Markert <pmarkert@adconion.com>
2014-09-08 09:51:03 -0700

    Added Procfile and bootstrap app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var morgan = require("morgan");
+var http = require("http");
+var request = require("request");
+var _ = require("underscore");
+
+var config = require("./lib/config")({
+	PORT : 3000,
+	YO_API_KEY : "INSERT KEY HERE",
+	YO_API_SEND_URL : "https://api.justyo.co/yo/"
+});
+
+var app = express();
+app.use(morgan('dev'));
+app.use("/", express.static(__dirname + "/public"));
+
+// GET Endpoint triggered as the yo application callback
+// YO will call this and supply the username as a querystring
+app.get("/yo", function(req, res) {
+	console.log("Thanks.");
+});
+
+app.listen(config.get("PORT"), function(err) {
+	if(err)
+		return console.error("Error - " + err);
+	console.log("Listening on " + config.get("PORT"));
+});
